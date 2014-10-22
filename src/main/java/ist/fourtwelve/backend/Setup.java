47fa9c59cf08885897f2867212af15074d0f880b
Steve <onethathasnolife@gmail.com>
2014-10-21 20:50:27 -0400

    Merge branch 'development' of C:\Users\Steve\IdeaProjects\crazy-octo-cat with conflicts.

--- src/main/java/ist/fourtwelve/backend/Setup.java
+++ src/main/java/ist/fourtwelve/backend/Setup.java
@@ -1,13 +1,8 @@
 package ist.fourtwelve.backend;
 
 import ist.fourtwelve.models.Project;
-import ist.fourtwelve.views.TestProjectGenerationView;
 
-import javax.swing.*;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 /**
  * Created by Steve on 10/19/2014.
@@ -21,7 +16,6 @@ public class Setup {
     private RunInfo info;
     private Project project;
     private ArrayList<Project> projects;
-    private JFrame frame;
     /*
     Original Setup, Do not use if methods are called inside
     TODO: Change this method to something concrete and not using test methods.
@@ -61,25 +55,6 @@ public class Setup {
     public void runProjects(){
 
     }
-    public void setFrame(JFrame x){
-        frame = x;
-    }
-    public void setGuiOutput(){
-        TestProjectGenerationView x = (TestProjectGenerationView) frame;
-        File outputFile = new File(info.outputFileName);
-        Scanner myScanner = null;
-        try {
-            myScanner = new Scanner(outputFile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        String tempString = "";
-        while(myScanner.hasNextLine())
-        {
-            tempString += myScanner.nextLine() + "\n";
-        }
-        x.setOutputArea(tempString);
-    }
     /*
     Compiles then runs mulitiple projects
      */
@@ -95,29 +70,42 @@ public class Setup {
                 info.setClassPath(temp.getSrcDir());
                 info.setArgs(temp.getTestRuns().get(j).getInputs());//Setup of run arguments-- This might be where the information isnt getting passed correctly.
                 info.setInputFileStub(info.getStudentPath());
-                info.setFrame(this.frame);
+
                 if(temp.getTestRuns().size() > 1){
-                    //IF there more than one test run for a project... do more stuff... else...
                     //TODO: If there are more args for the file? Not sure what to do here.
                 }
                 System.out.println("Path where the files should be located is... also unzipped to" + info.getStudentPath());
                 //TODO: Unzip some stuff
-                //TODO: Add in a way to put test arguments not from the text file -- Inputs done.
-                //TODO: Change the way RunJava handles the arguments. Should be something from the simple test case.
-                //TODO: Success needs to be changed in the project when the stuff below
-                //TODO: Way to get around unzipping is to unzip and then just move the damned files where they need to go.
+                //TODO: Success needs to be changed in the project when the stuff below -- Kinda done, handled.
+                //TODO: Handle args and scanner input separately when GUI gets the stuff here.
                 //make sure it doesnt contain a / in the front of the uh srcDir
                 info.setOutputFileName(info.getStudentPath() + "/output-" + info.getStudentName()+ ".txt");
                 compiler = new Compiler(info);
                 //Synchronized is probably not needed, but could work?
                 synchronized (compiler) {
-                    compiler.compileJava();
+                    int x = compiler.compileJava();
+                    boolean y;
+                    if(x == 0){
+                        y = true;
+                    }
+                    else {
+                        y = false;
+                    }
+                    projects.get(i).reportCompileSuccess(y); //Untested
                 }
                 runner = new RunJava(info);
                 synchronized (runner) {
-                    runner.runJava();
+                    int x = runner.runJava();
+                    boolean y;
+                    if (x == 0){
+                        y = true;
+                    }
+                    else {
+                        y = false;
+                    }
+                    projects.get(i).getTestRuns().get(j).reportRunSuccess(y); //Untested
                 }
-                setGuiOutput();
+
 
             }
 
