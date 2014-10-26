0ef53d63e1dcca1146f053c3f847db20463b3e66
jsr5194 <unkn0wn@client-75-102-67-163.mobility-up.psu.edu>
2014-10-26 14:30:18 -0400

    stubbed out Output.java **may not run until Requirements.java is complete

--- src/main/java/ist/fourtwelve/backend/Output.java
+++ src/main/java/ist/fourtwelve/backend/Output.java
@@ -1,24 +1,51 @@
 package ist.fourtwelve.backend;
 
 /**
- * Created by Steven on 10/26/2014.
+ * Created by jrittle on 10/26/2014.
  */
 public class Output {
 
+    private String outputString;
+    private Requirements requirementsObject;
+    private boolean didCompile;
+    private boolean didRun;
+
+    //constructors
     public Output(){
-
-    }
-    private String outputString ( ){
-        return null;
-    }
-    public Requirements getRequirments(){
-        return null;
-    }
-    public boolean didCompile(){
-        return false;
-    }
-    public boolean didRun(){
-        return false;
+        //meant to be empty
     }
 
+    //accessors
+    public String getOutputString(){
+        return this.outputString;
+    }
+
+    public Requirements getRequirements(){
+        return requirementsObject;
+    }
+
+    public boolean getDidCompile(){
+        return didCompile;
+    }
+
+    public boolean getDidRun(){
+        return didRun;
+    }
+
+    //mutators
+    public void setOutputString(String passedOutputString){
+        this.outputString = passedOutputString;
+    }
+
+    public void setRequirements(Requirements passedRequirements){
+        this.requirementsObject = passedRequirements;
+    }
+
+    public void setDidCompile(boolean passedDidCompile){
+        this.didCompile = passedDidCompile;
+    }
+
+    public void setDidRun(boolean passedDidRun){
+        this.didRun = passedDidRun;
+    }
 }
