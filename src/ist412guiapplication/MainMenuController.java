d93793db8bf9e2df46a3da66f297f00029853d4b
Tom Pazamickas <tpzap@yahoo.com>
2014-10-29 15:21:24 -0400

    GUI Push

---
+++ src/ist412guiapplication/MainMenuController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ist412guiapplication;
+
+/**
+ *
+ * @author cbald6
+ */
+public class MainMenuController {
+    
+    Management mainMenuView;
+    CourseCreationUI createCourseView;
+    ProjectTemplateManager projectTemplateView;
+    
+    public MainMenuController(){
+        mainMenuView = new Management();
+        
+    }
+    
+}
