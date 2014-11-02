d7d9f0d128011dd5a525cce359a6e9205c6eb983
stephenjohnwrightfarrington <stephenjohnwrightfarrington@client-75-102-111-116.mobility-up.psu.edu>
2014-11-02 16:09:29 -0500

    stephen farrington's changes to the javadocs for course.java inside the ist.fourtwelve.backend folder

--- src/main/java/ist/fourtwelve/models/Course.java
+++ src/main/java/ist/fourtwelve/models/Course.java
@@ -7,54 +7,101 @@
 package ist.fourtwelve.models;
 
 import java.util.ArrayList;
+
+/**
+ * Course contains an instructor, a studentList and a courseID, simply contains this information
+ */
 public class Course {
-    
+    /** the studentList is the arrayList of students*/
     private ArrayList<Student> theStudentList;
+    /** theInstructor is the instructor of the course*/
     private Instructor theInstructor;
+    /** courseID is the identity number of the course*/
     private int courseID;
-    
-    //Start Constructors.
+
+    /**
+     * this course constructor creates a course with an instructor, a list of students and a courseID
+     * @param whoIsTeaching is the instructor object
+     * @param studentList is the arrayList of students
+     * @param ID is the courseID integer
+     */
     public Course(Instructor whoIsTeaching, ArrayList<Student> studentList, int ID){
         theInstructor = whoIsTeaching;
         theStudentList = studentList;
         courseID = ID;
     }
+
+    /**
+     * this course constructor creates a course with an instructor and a courseID
+     * @param whoIsTeaching is the instructor object
+     * @param ID is the coureID integer
+     */
     public Course(Instructor whoIsTeaching, int ID){
         theInstructor = whoIsTeaching;
         courseID = ID;
         theStudentList = new ArrayList();
     }
-    
-    //End Constructors.
-    
-    //Start Get Methods.
+
+    /**
+     * getStudentList returns the arrayList of students
+     * @return the current arrayList of students in the course
+     */
     public ArrayList<Student> getStudentList(){
         return theStudentList;
     }
+
+    /**
+     * getCourseID returns the courseID integer
+     * @return the current courseID integer
+     */
     public int getCourseID(){
         return courseID;
     }
+
+    /**
+     * getInstructor returns the instructor object
+     * @return the current instructor object
+     */
     public Instructor getInstructor(){
         return theInstructor;
     }
-    //End Get Methods.
-    
-    //Start Set Methods.
+
+    /**
+     * setStudentList sets the arrayList of students
+     * @param newStudentList is the new arrayList of students
+     */
     public void setStudentList(ArrayList<Student> newStudentList){
         theStudentList = newStudentList;
     }
+
+    /**
+     * setCourseID sets the course ID number for the course.
+     * @param newID Relates to the given ID number you want the course to be.
+     */
     public void setCourseID(int newID){
         courseID = newID;
     }
+
+    /**
+     * setInstructor Sets/updates the course with a given instructor
+     * @param newInstructor The given instructor you want the course to have
+     */
     public void setInstructor(Instructor newInstructor){
         theInstructor = newInstructor;
     }
-    //End Set Methods.
-    
-    //Start Miscellaneous Methods.
+
+    /**
+     * studentToAdd Adds the student to the list of the students.
+     * @param studentToAdd The given student you want added to the course
+     */
     public void addStudentToList(Student studentToAdd){
         theStudentList.add(studentToAdd);
     }
+
+    /**
+     * 
+     * @param studentID The given ID you want the student to have, or the given ID of a student in the course.
+     */
     public void removeStudentFromList(int studentID){
         theStudentList.remove(studentID);
     }
