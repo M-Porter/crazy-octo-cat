f3f0eb24c7a3747257a1de45924dd213e2f9ae06
jsr5194 <unkn0wn@unkn0wns-MacBook-Pro.local>
2014-11-02 17:13:36 -0500

    added javadoc for professor and project model classes

--- src/main/java/ist/fourtwelve/models/Professor.java
+++ src/main/java/ist/fourtwelve/models/Professor.java
@@ -1,59 +1,82 @@
 package ist.fourtwelve.models;
 
-
+/**
+ * Professor, typically refered to a course professor in the college of IST
+ */
 public class Professor {
 
-    //Login Credentials
+    /** a login username for the professor */
     private String username;
+    /** a login password for the professor */
     private char[] password;
 
-    //Contact information\Password Confirmation
+    /** contact information for the professor */
     private String email;
 
-    //List of courses the Instructor is responsible for grading
-//    private ArrayList<Course> courseList;
-
-    //init with just username and password fields
+    /**
+     * Partial Constructor of a professor. Setup of some of the attributes that make up a professor
+     * @param username login username of the professor
+     * @param password login password of the professor.
+     */
     public Professor(String username, char[] password){
         this.username = username;
         this.password = password;
     }
 
-    //init with the additional email field
+    /**
+     * Full Constructor of a professor. Setup of all the attributes that make up a professor
+     * @param username login username of the professor
+     * @param password login password of the professor.
+     * @param email contact email for the professor.
+     */
     public Professor(String username, char[] password, String email){
         this.username = username;
         this.password = password;
         this.email    = email;
     }
 
-    //GET/SET Username
+    /**
+     * getUsername returns the username of the professor
+     * @return the login username of the professor
+     */
     public String getUsername(){
         return this.username;
     }
+    /**
+     * setUsername sets the username of professor
+     * @param username is the login username of the professor
+     */
     public void setUsername(String username){
         this.username = username;
     }
 
-
-    //GET/SET Password
+    /**
+     * getPassword returns the password of the professor
+     * @return the login password of the professor
+     */
     public char[] getPassword(){
         return this.password;
     }
+    /**
+     * setPassword sets the password of professor
+     * @param password is the login password of the professor
+     */
     public void setPassword(char[] password){
         this.password = password;
     }
 
-    //GET/SET Email
+    /**
+     * getEmail returns the email of the professor
+     * @return the contact email of the professor
+     */
     public String getEmail(){
         return this.email;
     }
+    /**
+     * setEmail sets the email of professor
+     * @param email is the contact email of the professor
+     */
     public void setEmail(String email){
         this.email = email;
     }
-
-    //GET courseList
-//    public ArrayList<Course> getCourseList(){
-//        return this.courseList;
-//    }
-
 }
--- src/main/java/ist/fourtwelve/models/Project.java
+++ src/main/java/ist/fourtwelve/models/Project.java
@@ -1,7 +1,3 @@
-/**
- *      TODO:
- */
-
 package ist.fourtwelve.models;
 
 import java.util.ArrayList;
@@ -10,30 +6,32 @@ import java.util.ArrayList;
  * @author cmb5837 Christopher M. Baldwin
  */
 
-
+/**
+ * Project, typically a java project submitted by an undergraduate of IST
+ */
 public class Project {
-    // ** INSTANCE VARIABLES **
-    //** DESCRIPTORS
-    // Name of the project
+    /** a name for the project */
     private String projectName;
 
-
-    //** INPUT
-    //Source directory from which the project will be compiled
+    /** a source directory from which the project will be compiled */
     private String srcDirPath;
 
-    //Name of the main class to be compiled
+    /** the Name of the main class to be compiled */
     private String mainClassName;
 
-    //Contains the input params for each testrun && stores output/results
+    /** an array list containing the input params for each testrun & stores output/results */
     private ArrayList<TestRun> testRuns;
 
-
-    //** STATUS
-    //boolean indicating run success *(contains true if any one test compiles)*
+    /** a boolean indicating run success (contains true if any one test compiles) */
     private boolean compileSuccess;
 
-    // ** CONSTRUCTORS **
+    /**
+     * Full Constructor of a Project. Setup of all the attributes that make up a project
+     * @param name the name of the project
+     * @param srcPath the path to the source files of the project
+     * @param mainClass the name of the main class of the project
+     * @param tests an array list of the tests for the project
+     */
     public Project(String name, String srcPath,String mainClass, ArrayList<TestRun> tests){
         this.projectName    = name;
         this.srcDirPath     = srcPath;
@@ -42,55 +40,85 @@ public class Project {
 
 
     }
-// ** CORE METHODS **
 
-// ** GETTERS AND SETTERS **
-
-    //** DESCRIPTORS
-    //Store the name
-    public void setName(String name){
-        this.projectName = name;
-    }
-    //Get the name
+    /**
+     * getName returns the name of the project
+     * @return the name of the project
+     */
     public String getName(){
         return this.projectName;
     }
-
-    //Store the name of the main class
-    public void setMainClassName(String name){
-        this.mainClassName = name;
+    /**
+     * setName sets the name of the project
+     * @param name is the name to be given to the project
+     */
+    public void setName(String name){
+        this.projectName = name;
     }
-    //Get the name of the main class
+
+    /**
+     * getMainClassName returns the name of the main class of the project
+     * @return the name of the main class of the project
+     */
     public String getMainClassName(){
         return this.mainClassName;
     }
-
-    //** INPUT
-    // set the file path where the project will be found
-    public void setSrcDir(String filepath){
-        this.srcDirPath = filepath;
+    /**
+     * setMainClassName sets the name of the main class of the project
+     * @param name is the name to be given to the main class of the project
+     */
+    public void setMainClassName(String name){
+        this.mainClassName = name;
     }
-    // return the file path the project is currently pointing to
+
+    /**
+     * getSrcDir returns the directory of the source files of the project
+     * @return the directory of the source files of the project
+     */
     public String getSrcDir(){
         return this.srcDirPath;
     }
-
-    public void setTestRuns(ArrayList<TestRun> testRuns){
-        this.testRuns = testRuns;
+    /**
+     * setSrcDir sets the directory of the source files of the project
+     * @param filepath is the directory of the source files of the project
+     */
+    public void setSrcDir(String filepath){
+        this.srcDirPath = filepath;
     }
+    
+    /**
+     * getTestRuns returns an array list of the test runs for the project
+     * @return an array list of the test runs for the project
+     */
     public ArrayList<TestRun> getTestRuns(){
         return this.testRuns;
     }
-
-    //** STATUS
-    //Use to report whether the test was a success or failure
-    public void reportCompileSuccess(boolean result){
-        this.compileSuccess = result;
+    /**
+     * setTestRuns sets the test runs for the project
+     * @param testRuns is an array list of the test runs for the project
+     */
+    public void setTestRuns(ArrayList<TestRun> testRuns){
+        this.testRuns = testRuns;
     }
-    //retrieve success of failure of this run, did program crash?
+    
+    /**
+     * getCompileSuccess returns a boolean indicating whether or not the project compiled
+     * @return a boolean indicating whether or not the project compiled
+     */
     public boolean getCompileSuccess(){
         return this.compileSuccess;
     }
+    //** STATUS
+    //Use to report whether the test was a success or failure
+
+    /**
+     * reportCompileSuccess set the compilation success indicator
+     * @param result is a boolean indicating whether or not the project compiled
+     */
+    public void reportCompileSuccess(boolean result){
+        this.compileSuccess = result;
+    }
+    
 
 
 
