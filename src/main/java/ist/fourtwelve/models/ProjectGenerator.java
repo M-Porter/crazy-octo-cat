d5187ec2ad52faed9514960f3f749e6baa29a236
Steven Yencho <onethathasnolife@gmail.com>
2014-11-02 16:33:43 -0500

    More java related documentation.

--- src/main/java/ist/fourtwelve/models/ProjectGenerator.java
+++ src/main/java/ist/fourtwelve/models/ProjectGenerator.java
@@ -11,35 +11,46 @@ import ist.fourtwelve.views.TestProjectGenerationView;
 import java.util.ArrayList;
 
 /**
- *
- * @author SickRebelSoul
+ * Generates certain information related to the project being handled by the gui.
  */
 public class ProjectGenerator {
-    
+    /**View generated from the class that to be updated and used*/
     private final TestProjectGenerationView genView;
+    /**ArrayList of projects being held by the class*/
     private ArrayList<Project> projectsList;
+    /**ArrayList of test runs related to the projects*/
     private ArrayList<TestRun> testRunsToAdd;
+    /**ArrayList of params that are used in the test runs*/
     private ArrayList<String> paramsToAdd;
-    
+    /**
+     * Default Constructor. Sets up the attributes of the class with instances of the variables
+     */
     public ProjectGenerator(){
         projectsList    = new ArrayList<Project>();
         testRunsToAdd   = new ArrayList<TestRun>();
         paramsToAdd     = new ArrayList<String>();
         genView = new TestProjectGenerationView(this);
     }
-    
+    /**
+     * Gets the current project list for the session
+     * @return Returns the project list that is currently generated.
+     */
     public ArrayList<Project> getProjList(){return this.projectsList;}
-    
-    public ArrayList<TestRun> getTestRunsList(){
-        return this.testRunsToAdd;
-    }
-    public ArrayList<String> getParamList(){
-        return paramsToAdd;
-    }
-    
+    /**
+     * Gets the current set of test run lists during this session.
+     * @return Returns the test run list that is currently generated.
+     */
+    public ArrayList<TestRun> getTestRunsList(){return this.testRunsToAdd;}
+    /**
+     * Gets the current set of parameters that was generated in this session
+     * @return Returns the parameter list that is currently generated.
+     */
+    public ArrayList<String> getParamList(){return paramsToAdd;}
+    /**Resets the test runs that are currently generated.*/
     public void resetTestRuns(){
         this.testRunsToAdd.clear();
     }
+    /** Resets the paramters tha tare currently generated. */
     public void resetParams(){
         this.paramsToAdd.clear();
     }
--- src/main/java/ist/fourtwelve/models/Student.java
+++ src/main/java/ist/fourtwelve/models/Student.java
@@ -7,41 +7,51 @@
 package ist.fourtwelve.models;
 import java.util.ArrayList;
 
-//Author Tom Paz
-
+/**
+ * Student, typically refered to an undergraduate in the college of IST
+ */
 public class Student {
-
+    /**ArrayList of projects the student belongs to*/
     private ArrayList<Project> theProjectList;
+    /**A number the student can be identified by*/
     private int studentID;
+    /**A way to personally identify a student */
     private String firstName;
+    /**A formal way to personally identify a student properly, goes along with the firstName*/
     private String lastName;
-    //Reference back to the course the student is a part of. 
+    /**
+     * Full Constructor of a student. Setup of all the attributes that make up a student
+     * @param fName firstName of the student
+     * @param lName lastName of the student.
+     * @param ID studentID of the student.
+     * @param projects List of projects the student is currently engaged with.
+     */
     public Student(String fName, String lName, int ID, ArrayList<Project> projects){
         firstName = fName;
         lastName = lName;
         studentID = ID;
         theProjectList = projects;
     }
+    /**
+     * Partial constructor of student. Set of the main information for the student.
+     * @param fName firstName of the student
+     * @param lName lastName of the student.
+     * @param ID studentID of the student.
+     */
     public Student(String fName, String lName, int ID){
         firstName = fName;
         lastName = lName;
         studentID = ID;
         theProjectList = new ArrayList();
     }
-    
-    //Getters
-    public String getFirstName(){
-        return firstName;
-    }
-    public String getLastName(){
-        return lastName;
-    }
-    public int getStudentID(){
-        return studentID;
-    }
-    public ArrayList<Project> getStudentProject(){
-        return theProjectList;
-    }
+    /**
+     *
+     * @return
+     */
+    public String getFirstName(){ return firstName;}
+    public String getLastName(){return lastName;}
+    public int getStudentID(){return studentID;}
+    public ArrayList<Project> getStudentProject(){return theProjectList;}
     
     
 }
